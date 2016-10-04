83e4669649fc215dbda8c4888e6389f8e08f2b0b
Gibberish314 <a.moosen@gmail.com>
2016-10-04 11:59:39 -0400

    Add edit course.

--- app/app.php
+++ app/app.php
@@ -27,6 +27,9 @@
     $app->get("/", function() use($app) {
         return $app['twig']->render('index.html.twig');        
     });
+
+// COURSES
+
     $app->get("/courses", function() use($app) {
         return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));        
     });
@@ -35,6 +38,22 @@
         return $app['twig']->render('course.html.twig', array('single_course' => Course::find($course_id)));        
     });
 
+
+    $app->get("courses/{course_to_edit_id}/edit", function($course_to_edit_id) use($app) {
+        $current_course = Course::find($course_to_edit_id);
+        return $app['twig']->render('edit-course.html.twig', array('editing_course' => $current_course));        
+    });
+
+    $app->patch("/courses/{course_id}", function($course_id) use($app) {
+        // $test_course->update($new_name);
+        $current_course = Course::find($course_id);
+        $current_course->update($_POST['new_name']);
+        return $app['twig']->render('course.html.twig', array('single_course' => $current_course));        
+    });
+
+
+
+// STUDENTS
 
     $app->get("/students", function() use($app) {
         return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));        
